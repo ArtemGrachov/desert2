be8a417bf60fffb3f66b84b50b3b9f88d17d28b2
Artem Grachov <grachov2195@gmail.com>
2017-06-29 11:29:37 +0300

    Welcome page: CSS + JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,20 @@
-console.log('test');
+var authWindow = function() {
+    let showGreet = $('.show-greet'),
+        showAuth = $('.show-auth');
+
+
+    showAuth.on('click', function(e) {
+        e.preventDefault();
+        $('.window').toggleClass('window_rotate');
+        showAuth.toggleClass('btn-auth_disabled')
+    })
+
+    showGreet.on('click', function(e) {
+        e.preventDefault();
+        $('.window').toggleClass('window_rotate');
+        showAuth.toggleClass('btn-auth_disabled')
+    })
+}
+$(document).ready(function() {
+    authWindow();
+})
