3384ba77cd1fc76199f9fe3b30aa3d67d67f38f4
Artem Grachov <grachov2195@gmail.com>
2017-06-30 12:40:30 +0300

    Works & About pages. HTML, CSS, JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-var authWindow = function() {
+let authWindow = function() {
     let showGreet = $('.show-greet'),
         showAuth = $('.show-auth');
 
@@ -15,6 +15,53 @@ var authWindow = function() {
         showAuth.toggleClass('btn-auth_disabled')
     })
 }
+
+let scrollBottom = function() {
+    $('.view-toggle__btn_down').on('click', function(e) {
+        e.preventDefault();
+        let scrollPos = $('.header').height();
+        $('body, html').animate({ scrollTop: scrollPos }, 1500);
+    })
+    $('.view-toggle__btn_up').on('click', function(e) {
+        e.preventDefault();
+        $('body, html').animate({ scrollTop: 0 }, 1500);
+    })
+}
+
+let toggleNav = function() {
+    $('.nav-toggle').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('nav-toggle_active');
+        $('.nav').fadeToggle();
+    })
+}
+
+
+let drawCircleChart = function(chartDiam, strokeWidth) {
+    let charts = $('.chart'),
+        chartRadius = (chartDiam - strokeWidth) / 2,
+        charPi = (chartDiam - strokeWidth) * Math.PI;
+
+    console.log(charPi);
+    for (let i = 0; i < charts.length; i++) {
+        let chartName = $(charts[i]).find('.chart__name').html(),
+            chartValue = $(charts[i]).find('.chart__value').html(),
+            chartPercentage = chartValue * charPi / 100,
+            chartHTML = `
+                <svg width="${chartDiam}" height="${chartDiam}">
+                    <circle transform="rotate(-90)" r="${chartRadius}" cx="-50%" cy="50%" stroke-width="${strokeWidth}"/>
+                    <circle transform="rotate(-90)" style="stroke-dasharray: ${chartPercentage }px ${charPi}px;" r="${chartRadius}" cx="-50%" cy="50%" stroke-width="${strokeWidth}"/>
+                    <text x="50%" fill="currentColor" y="50%" text-anchor="middle" dominant-baseline="middle">${chartName}</text>
+                </svg>
+            `
+        $(charts[i]).html(chartHTML);
+    }
+}
+
 $(document).ready(function() {
     authWindow();
+    scrollBottom();
+    toggleNav();
+    drawCircleChart(110, 20);
+
 })
