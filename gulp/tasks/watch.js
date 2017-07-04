0fc87966173d84021361735f8d1829ee7f8db3cd
Artem Grachov <grachov2195@gmail.com>
2017-07-04 17:52:13 +0300

    Mobile CSS. Some CSS fix (About page etc.)

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,7 @@
 module.exports = function() {
     $.gulp.task('watch', function() {
-        $.gulp.watch($.cfg.app + 'sass/**/*.scss', ['sass', $.browserSync.reload]);
+        // $.gulp.watch($.cfg.app + 'sass/**/*.scss', ['sass', $.browserSync.reload]);
+        // $.gulp.watch($.cfg.app + 'sass/**/*.scss', ['sass', gulp.src($.cfg.dist + 'css').pipe(browserSync.stram())]);
         $.gulp.watch($.cfg.app + 'pug/**/*.pug', ['pug', $.browserSync.reload]);
         $.gulp.watch($.cfg.app + 'js/**/*.js', ['scripts', $.browserSync.reload]);
     })
